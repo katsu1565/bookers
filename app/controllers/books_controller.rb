51cb113234b0d9a84fc813648d2bfd15880ba8aa
高寺克重 <takaderakatsue@takajikatsuenoMacBook-Air.local>
2020-09-08 21:54:51 +0900

    add

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+	def top
+	end
+
+	def index
+		@books = Book.all
+		@book = Book.new
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to books_path
+    end
+
+    private
+    def book_params
+    	params.require(:book).permit(:title, :body)
+    end
+end
